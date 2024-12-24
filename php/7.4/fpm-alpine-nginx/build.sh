f16b9ad87f5b48b88f5186fcdf13cac06faa62dd
z <z@qq.com>
2024-12-24 10:40:38 +0800

    dev

--- php/7.4/fpm-alpine-nginx/build.sh
+++ php/7.4/fpm-alpine-nginx/build.sh
@@ -1,1 +1,3 @@
-docker build -t php:7.4-cli-alpine-nginx ./
+docker build -t php:7.4.33-cli-alpine-nginx ./
+docker tag php:7.4.33-cli-alpine-nginx registry.cn-hangzhou.aliyuncs.com/php127/php:7.4.33-cli-alpine-nginx
+docker push registry.cn-hangzhou.aliyuncs.com/php127/php:7.4.33-cli-alpine-nginx
